560473d7575b538eb518887f782ade91a3c848b5
amynollz153@gmail.com <amynollz153@gmail.com>
2024-12-03 11:36:54 +0200

    Initialize New Project

---
+++ src/main/java/bohemian_excellence_digidemy/bohemian_excellence_digidemy/BohemianExcellenceDigidemyApplication.java
@@ -0,0 +1,13 @@
+package bohemian_excellence_digidemy.bohemian_excellence_digidemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BohemianExcellenceDigidemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BohemianExcellenceDigidemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bohemian_excellence_digidemy/bohemian_excellence_digidemy/BohemianExcellenceDigidemyApplicationTests.java
@@ -0,0 +1,13 @@
+package bohemian_excellence_digidemy.bohemian_excellence_digidemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BohemianExcellenceDigidemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
